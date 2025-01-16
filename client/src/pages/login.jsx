b252e2e9c211023422ddc0cf64cec03829a0c298
Eyapu Luke <eyapuluke278@gmail.com>
2025-01-16 03:57:49 +0300

    final commit

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -7,7 +7,7 @@ const Register = () => {
 
   return (
     <form action="" className="form-elements">
-      <h2>Register</h2>
+      <h2>Login</h2>
       <label htmlFor="username">Username: </label>
       <input type="text" id="username" placeholder="Enter new username" value={username}
       onChange={(event) => setUsername(event.target.value)}
@@ -16,7 +16,7 @@ const Register = () => {
       <input type="text" id="password" placeholder="Enter password" value={password}
       onChange={(event) => setPassword(event.target.value)}
       />
-      <button type="submit" className="nav-buttons-home">register</button>
+      <button type="submit" className="nav-buttons-home">Login</button>
     </form>
   )
 }
